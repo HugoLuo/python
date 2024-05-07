8bd0730a6d35e584cc80d55382de9d8e73adc9e3
罗海涛 <luohaitao@luohaitaos-MacBook-Pro.local>
2024-05-08 05:14:46 +0800

    add class extend

--- Python_Object.py
+++ Python_Object.py
@@ -19,7 +19,7 @@ class MyPeople:
 # oneObject.age=25
 # oneObject.print_info()
 
-
+# 定义一个普通类
 class Person:
     def __init__(self,name,age) :
         self.name=name
@@ -27,5 +27,88 @@ class Person:
     def print_info(self):
         print("My name is {0} and age is {1}".format(self.name,self.age))
 
-obj=Person("Jon",23)
-obj.print_info()
+# obj=Person("Jon",23)
+# obj.name="Domina"
+# obj.print_info()
+
+#__隐藏属性的使用
+class People:
+    def __init__(self,name,age=30):
+        self.__name=name
+        self.__age=age
+    def print_info(self):
+        print("My name is {0} age is {1}".format(self.__name,self.__age) )
+
+# people=People('Amy')
+# people.__name="Candy"
+#使用__隐藏属性，实例就无法直接操作
+# people.print_info()
+
+#类提供方法去访问或设置类属性
+class Zoo:
+    def __init__(self,name,age=1) -> None:
+        self.__name=name
+        self.__age=age
+    
+    def set_name(self,name):
+        self.__name=name
+    
+    def set_age(self,age):
+        self.__age=age
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_age(self):
+        return self.__age
+    
+    def print_info(self):
+        print("the zoo name is {0} age is {1}".format(self.__name,self.__age))
+
+# dog=Zoo('Dog',3)
+# dog.print_info()
+
+
+# 类的封装，隐藏的方法，方法智能在类内部使用
+class Animal:
+    def __init__(self,name,age) -> None:
+        self.__name=name
+        self.__age=age
+    
+    #通过在方法前加一个下划线定一个隐藏的方法，该方法只能被类内部使用。
+    #隐藏方法可以在类内部做一些内部操作或运算。
+    def _private(self):
+        print("内部方法被调用")
+    
+    #在一般的类方法里可以访问类内定义的隐藏方法
+    def call_private_func(self):
+        self._private()
+
+# animal=Animal("cat",1)
+# #调用类的一般方法
+# animal.call_private_func()
+
+
+#类的继承
+# class Programer(People):
+#     pass
+
+class Programer(People):
+    #在新的类中，定义自己独有的方法
+    def coding(self):
+        print("程序员有独有的技能coding")
+
+#使用新的类，创建实例对象
+programer=Programer("Hugo",42)
+
+#调用父类中的方法
+programer.print_info()
+
+#调用自己独有的方法
+programer.coding()
+
+
+
+
+
+
