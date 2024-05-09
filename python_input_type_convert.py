414c635d36bb1d2e9aee1b3f2187a4e0a6615ac3
罗海涛 <luohaitao@luohaitaos-MacBook-Pro.local>
2024-05-09 09:18:35 +0800

    添加获取参数argv相关的办法

---
+++ python_input_type_convert.py
@@ -0,0 +1,25 @@
+
+# 读取用户的输入，默认是接受字符串类型的
+# result=input18
+("Please input your result:")
+# print(result)
+
+# 不断接受用户的输入
+# while True:
+#     result=input("Pleae provide your input(type exit end session):")
+#     if result == 'exit':
+#         break
+
+
+# 数值与字符串间的转换
+str_age=input("Please provide your age:")
+print(type(str_age))
+age=int(str_age)
+print(type(age))
+print(age)
+
+number=18
+print(number)
+print(type(number))
+str_number=str(number)
+print(type(str_number))
---
+++ python_parameters_of_function.py
@@ -0,0 +1,23 @@
+# 编辑python文件  mypram.py
+import sys
+print(sys.argv)
+
+# 获取参数
+# sys.argv表示的含义
+print(sys.argv[0])  #获取python的文件名
+print(sys.argv[1])  #获取python文件的第一个参数，
+print(sys.argv[2])  #获取python文件的第二个参数
+print(sys.argv[1:])  #获取所有参数的列表
+print(len(sys.argv)-1) #获取参数的个数
+
+# 当执行上面程序时，向该程序传递参数的操作如下：
+# python3 mypram.py parameter1 parameter2
+
+
+# python3 python_parameters_of_function.py parameter1 parameter2
+# ['python_parameters_of_function.py', 'parameter1', 'parameter2']
+#传进python文件的参数将以列表的方式存储到一个名为argv的列表里
+
+
+
+
